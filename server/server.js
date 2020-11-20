8c101261f47db80fc5b1e3207df39bc28c542e9d
elie.fossi <elie.fossi@gmail.com>
2020-11-20 04:46:53 -0500

    Latest changes

--- server/server.js
+++ server/server.js
@@ -8,10 +8,12 @@ const URL = require('url');
 require('log-timestamp');
 const upload = multer();
 const app = express();
+const AWS = require("aws-sdk");
 
 const MAUTIC_BASE_URL = process.env.MAUTIC_URL || 'https://m2.beebl.io';
 const MAUTIC_FORM_ID = process.env.MAUTIC_FORM_ID || '3';
-
+const NB_ATTEMPTS = 5;
+const MILLIS_BETWEEN_ATTEMPS = 2*60*1000 ; //millis
 
 app.get('/', (req, res) => {
   res.send('Hello from Node!');
@@ -26,8 +28,8 @@ app.listen(PORT, () => {
 // app.use(bodyParser.urlencoded({ extended: true }));
 //app.use(bodyParser.json());
 
-app.use(upload.array()); 
-app.use(express.static('public'));
+app.use('/apify', [ upload.array(), express.static('public') ] ); 
+app.use('/ses', bodyParser.json());
 
 app.post('/apify', (req, res) => {
 
@@ -40,11 +42,16 @@ app.post('/apify', (req, res) => {
       saveContactDetailsToDB(contactList, req.body.url);
       // postContactToForm(contactList,MAUTIC_FORM_ID);
     }
-  	res.send({contactList:contactList, details:contactDetails});
+  	res.status(200).send({contactList:contactList, details:contactDetails});
   }else{
-  	res.send({});	
-  }  
+  	res.status(404).json({
+      success: false,
+      message: 'missing htmlParameter'
+    });	
+  }
 });
+
+
 
 
 const extractNameFromEmail = (email, separatingChar)=>{
@@ -101,30 +108,92 @@ const extractNameFromEmailCamelCase = (email)=>{
   return {fn:firstName, ln:lastName, mn:middleName};
 }
 
+const acceptableEmail = async (email) => {
+
+ // To remove emails like
+ // rizwest1975@yahoo.comm    
+ // Hotro.ttvnol@gmail.comli    
+  const reToExclude = ['@.*\.com[a-zA-Z0-9]+'];
+
+
+  const acceptableDomains = ['comcastbiz.net', 
+                           '.company', 
+                           'comperia.pl', 
+                           'commnet.edu'];
+
+  const prohibitedDomains = [
+                           //  '@mail.ru', 
+                           // '@inbox.ru', 
+                           // '@gpsmcard.ru',
+                           // '@shakespeare-online.com'
+                           ];
+                           
+  // Exclude email from prohibitedDomains, then
+  // Exclude emails that match any re in reToExclude 
+  // unless the email ends in a domain in acceptableDomains
+  const prohibitedD = prohibitedDomains.filter( d => email.toLowerCase().endsWith(d.toLowerCase()) );
+  if ( prohibitedD && prohibitedD.length > 0){
+    return false;
+  }
+
+// Make sure the email is not on the do-not-contact list
+  
+  const doNotContact = await promiseOfQuery('select l.email from leads l join lead_donotcontact dnc on l.id = dnc.lead_id where l.email = ?', email);
+  console.log('doNotContact result:',doNotContact);
+  if ( doNotContact ){
+    console.log('Fount email in do-not-contact', email);
+    return false;
+  }
+  // const prohibitedN = prohibitedEmailMatches.filter( d => email.toLowerCase().indexOf(d) > -1 );
+  // if ( prohibitedN && prohibitedN.length > 0){
+  //   return false;
+  // }
+
+  const domains = acceptableDomains.filter( a => email.endsWith(a) );
+  if (domains && domains.length > 0){
+    return true;
+  }else{
+    const res = reToExclude.filter( ( re ) => email.match(new RegExp(re) ));
+    if ( res && res.length > 0){
+      return false;
+    }else{
+      return true;
+    }
+  }
+}
+
+
+
 const contactDetails2List = (contactDetails, url) =>{
   const contactList = [];
+
+
   Object.keys(contactDetails).forEach(function(key) {
     const values = contactDetails[key];
 
     if ( key === 'emails' && values && values.length ){
       values.forEach(v=>{
-      //for(const v in values){
-        let emailDetails = extractNameFromEmail(v, '.');
+        // Filter out invalid email
+        if(v && acceptableEmail(v) ){
+          let emailDetails = extractNameFromEmail(v, '.');
 
-        if( !emailDetails.fn && !emailDetails.ln) {
-          emailDetails = extractNameFromEmail(v, '_');
-        }
+          if( !emailDetails.fn && !emailDetails.ln) {
+            emailDetails = extractNameFromEmail(v, '_');
+          }
 
-        if( !emailDetails.fn && !emailDetails.ln) {
-          emailDetails = extractNameFromEmailCamelCase(v);
+          if( !emailDetails.fn && !emailDetails.ln) {
+            emailDetails = extractNameFromEmailCamelCase(v);
+          }
+          contactList.push({
+            email: v,
+            firstname_unsure: emailDetails.fn,
+            lastname_unsure: emailDetails.ln,
+            middlename_unsure: emailDetails.mn,
+            company_unsure: (url && url.length>0) ? URL.parse(url).hostname : ''
+          });
+        }else {
+          console.log("Unacceptable email: ", v);
         }
-        contactList.push({
-          email: v,
-          firstname_unsure: emailDetails.fn,
-          lastname_unsure: emailDetails.ln,
-          middlename_unsure: emailDetails.mn,
-          company_unsure: (url && url.length>0) ? URL.parse(url).hostname : ''
-        });
       });
     }
 
@@ -164,7 +233,6 @@ const contactDetails2List = (contactDetails, url) =>{
         contactList.push({googleplus:v});
       })
     }
-
   });
   return contactList;
 }
@@ -226,10 +294,25 @@ const saveContactDetailsToDB = (contactList, url)=>{
 
 }
 
+
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-const postContactToForm = ( contactList, formId )=>{
-  contactList.forEach( contact => {
+const setDoNotContact = async ( contact )=>{    
+  //Only contacts with email
+  if ( contact.email ){
+    try{
+
+      const insertQuery = `insert into lead_donotcontact(lead_id , date_added , reason , channel , channel_id , comments) values((select id from leads where email='${contact.email}'),now(),'1','email',39,'User unsubscribed by external API');`;
+      const queryRes = await promiseOfQuery ( insertQuery, null );
+      return queryRes;    
+
+    }catch(e){
+      console.log('Error posting form:',e);
+    }
+  }
+}
+
+const postContactToForm = async ( contact, formId )=>{
     const form = new FormData();
     form.append('mauticform[formId]',formId);
     form.append('mauticform[messenger]','1');
@@ -237,52 +320,19 @@ const postContactToForm = ( contactList, formId )=>{
     if( contact.email ){
       form.append('mauticform[email]',contact.email);
     }
-
-    if( contact.firstname_unsure ){
-      form.append('mauticform[first_name]',capitalize(contact.firstname_unsure));
-    }
-    if( contact.company_unsure ){
-      form.append('mauticform[company]',contact.company_unsure);
-    }    
-    if( contact.lastname_unsure ){
-      form.append('mauticform[last_name]',capitalize(contact.lastname_unsure));
-    }
-    if( contact.phone ){
-      form.append('mauticform[phone]',contact.phone);
-    }
-    if( contact.linkedin ){
-      form.append('mauticform[linkedin]',contact.linkedin);
-    }
-    if( contact.twitter ){
-      form.append('mauticform[twitter]',contact.twitter);
-    }
-    if( contact.instagram ){
-      form.append('mauticform[instagram]',contact.instagram);
-    }
-    if( contact.facebook ){
-      form.append('mauticform[facebook]',contact.facebook);
-    }
-    if( contact.googleplus ){
-      form.append('mauticform[googleplus]',contact.googleplus);
-    }
-    
-    //Only post contacts with email
+       
+    //Only  contacts with email
     if ( contact.email ){
       try{
 
-        form.submit( MAUTIC_BASE_URL + '/form/submit', function(err, res) {
-          if (err) {
-            console.error("ERROR posting form: "+JSON.stringify(err));
-          };
-          console.log('Form posted: statusCode:',res.statusCode,' res.data:', res.data);
-        } );       
+        const formResponse =  await promiseOfFormData(form, MAUTIC_BASE_URL + '/form/submit' );
+        return formResponse;    
 
       }catch(e){
         console.log('Error posting form:',e);
       }
     }
 
-  })
 }
 
 const insertIntoDB = (table, columns, values)=>{
@@ -319,3 +369,207 @@ const insertiontQuery = (table, columns, values)=>{
 }
 
 
+const promiseOfFormData = ( form, url ) => {
+  return new Promise(function (resolve, reject) {
+
+    form.submit( url, function(err, res) {
+
+      if (err || res.statusCode != 200 ) {
+        console.error("ERROR posting form: ", err, res);
+        if (err)
+          reject(err);
+        else
+          resolve({ status: res.statusCode});
+      };
+      console.log('Form posted: statusCode:',res.statusCode,' res.data:', res.data);
+      resolve(res);
+    } );    
+  });
+}
+
+const promiseOfQuery = ( query, params ) => {
+  return new Promise(function (resolve, reject) {
+
+    console.log("runQueryToDB - query, params:", query, params);
+    mysqlDb.query(query, params, function (data, error) {       
+      if (error){
+
+        console.log("Error while running query: "+ JSON.stringify(error));
+        reject(error);
+      }else{
+        resolve(data);
+        console.log("Query result:", table, JSON.stringify(data));
+      }
+    });    
+  });
+}
+
+
+
+
+
+
+
+
+AWS.config.update({
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+  region: "us-east-1"
+});
+
+const sns = new AWS.SNS();
+
+const topicArnBounce = process.env.TOPIC_ARN_BOUNCE;
+var paramsTopicBounces = {
+  Protocol: "https",
+  TopicArn: topicArnBounce,
+  Endpoint: process.env.BOUNCE_ENDPOINT
+};
+
+const topicArnComplaint =process.env.TOPIC_ARN_COMPLAINT
+var paramsTopicComplaints = {
+  Protocol: "https",
+  TopicArn: topicArnComplaint,
+  Endpoint: process.env.COMPLAINT_ENDPOINT
+};
+
+sns.subscribe(paramsTopicBounces, function(error, data) {
+  if (error) throw new Error(`Unable to set up SNS subscription: ${error}`);
+  console.log(`SNS subscription set up successfully: ${JSON.stringify(data)}`);
+});
+
+sns.subscribe(paramsTopicComplaints, function(error, data) {
+  if (error) throw new Error(`Unable to set up SNS subscription: ${error}`);
+  console.log(`SNS subscription set up successfully: ${JSON.stringify(data)}`);
+});
+
+const handleSnsNotification = async (req, res) => {
+  console.log('handleSnsNotification req, res :',req, res);
+  const message = JSON.parse(req.body.Message);
+  console.log('handleSnsNotification message :',message);
+
+  if (
+    (message && message.notificationType == "Bounce") ||
+    message.notificationType == "Complaint"
+  ) {
+    const mail = message.mail;
+    if (mail && mail.destination) {
+      for (let i = 0; i < mail.destination.length; i++) {
+        const address = mail.destination[i];
+        try {
+//  Make n attempts to update the contact. If all the attempts all fail, 
+// send a email
+          for ( let j = 0 ; j < NB_ATTEMPTS ; j++ ) {
+            console.log(`Attempt ${j} to set DoNotContact for ${address}`);
+            const res = await setDoNotContact( {email:address } );
+            if (res){
+              return res;
+            }else {
+              await sleep (MILLIS_BETWEEN_ATTEMPS);
+            }
+            // If we get here, send an email
+            console.log(`Should send an email about failing to set DoNotContact for ${address}`);
+          }
+        } catch (error) {
+          console.error( "Error setting DoNotContact for ",address);
+          console.error(error.message);
+        }
+      }
+    }
+  }
+};
+
+const handleResponse = async (topicArn, req, res) => {
+  console.log('handleResponse topicArn, req, res',topicArn, req, res);
+  if (
+    req.headers["x-amz-sns-message-type"] === "Notification" &&
+    req.body.Message
+  ) {
+    await handleSnsNotification(req, res);
+  } else if (
+    req.headers["x-amz-sns-message-type"] === "SubscriptionConfirmation"
+  ) {
+    var params = {
+      Token: req.body.Token,
+      TopicArn: topicArn
+    };
+
+    sns.confirmSubscription(params, function(err, data) {
+      if (err) throw err; // an error occurred
+      console.error("handleResponse - data:", data);
+    });
+  }
+};
+
+
+
+app.post('/ses/do-not-contact', async (req, res) => {
+    console.log('do-not-contact req.body:',req.body);
+
+    if ( req && req.body && req.body.email ){
+      const email = req.body.email;
+
+      if ( email && email.includes("@") ) {
+        try{
+          const res = await setDoNotContact( {email:email } );
+          if (res){
+            res.status(200).json({
+              success: true,
+              message: res
+            });
+          }else {
+            res.status(404).json({
+              success: false,
+              message: 'Issue setting do-not-contact'
+            });
+          }
+        }catch(err){
+          console.log('error in do-not-contact req.body:',err);
+          res.status(500).json({
+              success: false,
+              message: err
+          });
+        }
+
+      }
+    }else{
+      res.status(404).json({
+        success: false,
+        message: "Missing email parameter"
+      });    
+    }
+
+
+});
+
+app.post('/ses/handle-bounces', async (req, res) => {
+  try {
+    await handleResponse(topicArnBounce, req, res);
+
+    res.status(200).json({
+      success: true,
+      message: "Successfully received message"
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+});
+
+app.post('/ses/handle-complaints', async (req, res) => {
+  try {
+    await handleResponse(topicArnComplaint, req, res);
+
+    res.status(200).json({
+      success: true,
+      message: "Successfully received message."
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+});
